7b2f907d347a3376fe6abf7b12aa176ed85acdd0
MrCommit <maxirybakin@gmail.com>
2022-03-08 22:20:56 -0800

    Commiting the final working draft of the note taking app

---
+++ server.js
@@ -0,0 +1,66 @@
+// Exporting dependencies
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
+
+
+// Setting up express server and PORT connection(s)
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+
+// Middleware for parsing JSON and URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// --- Setting API call routes ---
+// GET request for reading notes json DB - /db/db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+// POST function for adding new notes to json DB - /db/db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+// DELETE function for deleting notes based on note ID's
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+
+// --- Setting HTML resource routes ---
+// GET the Main page - public/index.html 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// GET the Notes page - public/notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// GET all the generic requests to display main page
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+
+// Listening for PORT connections 
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
